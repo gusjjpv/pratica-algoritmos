915da8e3ccd2e06cf1004e55908fca6ab819568f
Joao Gustavo <joaogustavolima084@gmail.com>
2023-11-29 15:10:50 -0300

    questão 2
    fiz a dois

---
+++ 02_questao.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int f(int mat[3][3])
+{
+    return (mat[0][0] * mat[1][1] * mat[2][2])
+         + (mat[0][1] * mat[1][2] * mat[2][0])
+         + (mat[0][2] * mat[1][0] * mat[2][1])
+         - (mat[0][1] * mat[0][1] * mat[2][2])
+         - (mat[0][0] * mat[1][2] * mat[2][1])
+         - (mat[0][2] * mat[1][1] * mat[2][0]);
+}
+
+int main(void)
+{
+    int m[3][3] = {{0, 1, 0}, {3, 1, -1}, {4, 0, 1}};
+    int de = f(m);
+    printf(" Resultado = \t %d", de);
+    return 0;
+}
+
+/*
+
+saida = -5
+
+o programa armazena uma matriz 3x3(3 linhas e 3 colunas), chama a função que ira multiplicar os valores dos indices especificados e em seguidas ira fazer as operações seguintes, depois é mostrasdo na tela 
+
+*/

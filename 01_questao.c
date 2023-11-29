319b0e72c43d787727ccdca576d279d4e7198903
Joao Gustavo <joaogustavolima084@gmail.com>
2023-11-29 16:10:55 -0300

    lista

--- 01_questao.c
+++ 01_questao.c
@@ -1,11 +1,25 @@
 #include <stdio.h>
-/*
-Implemente uma função que receba como parametro um vetor de numeros inteiros (vet) de tamanho n e inverta a ordem dos elementos armazenados nesse vetor.
-*/
 
-int main(void){
-    
+void inverterNum(int v[], int n)
+{
+    int i;
+    for (i = n-1; i > 0; i--)
+    {
+        printf("%d", v[i]);
+    }
+}
 
+int main()
+{
+
+    int i, n = 5;
+    int vet[n];
+    for (i = 0; i < n; i++)
+    {
+        vet[i] = i + 1;
+    }
+
+    inverterNum(vet[n], n);
 
     return 0;
 }

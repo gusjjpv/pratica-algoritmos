28d47a50e82943ad9f84028cf0ad1ebb14494dfa
Joao Gustavo <joaogustavolima084@gmail.com>
2023-11-29 13:19:09 -0300

    questao 1

---
+++ 01_questao.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/*
+Implemente uma função que receba como parametro um vetor de numeros inteiros (vet) de tamanho n e inverta a ordem dos elementos armazenados nesse vetor.
+*/
+
+int main(void){
+    
+
+
+    return 0;
+}
